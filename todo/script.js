b1935e135086a4c16cf6902f20edef2aab36a6e1
Sergey <dodzeek@gmail.com>
2020-10-16 21:19:20 +0500

    todo phone vue

--- todo/script.js
+++ todo/script.js
@@ -11,7 +11,7 @@ if (localStorage.getItem('todo')) {
 }
 
 input.addEventListener('keydown', e => {
-    if(e.key === 'Enter' || e.keyCode === 13){
+    if (e.key === 'Enter' || e.keyCode === 13) {
         pushInput()
     }
 })
@@ -20,16 +20,19 @@ addBtn.addEventListener('click', pushInput)
 
 function pushInput() {
     if (input.value.trim() !== '') {
-    todoList.unshift({
-        text: input.value,
-        done: false
-    })
-    input.value = ''
-    localStorage.setItem('todo', JSON.stringify(todoList))
-    drowList()} else {
+        todoList.unshift({
+            text: input.value,
+            done: false
+        })
+        input.value = ''
+        input.focus()
+        localStorage.setItem('todo', JSON.stringify(todoList))
+        drowList()
+    } else {
         alert('Напиши нормальный таск!')
         throw new Error('ти шо ебобо?')
-}}
+    }
+}
 
 
 
@@ -39,38 +42,39 @@ function drowList() {
 
     for (const todoItem of todoList) {
 
-    const liItem = document.createElement('li')
-    liItem.classList = 'list-item'
-    list.append(liItem)
-    
-    const pItem = document.createElement('p')
-    pItem.classList.add('list-content')
-    pItem.innerText = todoItem.text
-    if(todoItem.done){pItem.classList.add('taskDone')}
-    
-    const buttonDone = document.createElement('button')
-    buttonDone.classList.add('list-btn', 'done', 'btn')
-    buttonDone.innerText = 'done'
-    buttonDone.addEventListener('click', () => {
-        todoItem.done = !todoItem.done
-        drowList()
-    })
-    
-    const buttonRem = document.createElement('button')
-    buttonRem.classList.add('list-btn', 'remove', 'btn')
-    buttonRem.innerText = 'remove'
-    buttonRem.addEventListener('click', () => {
-        const curInd = todoList.indexOf(todoItem)
-        todoList.splice(curInd, 1)
-        if(todoList.length){
-        localStorage.setItem('todo', JSON.stringify(todoList))
-        drowList()}else {
-            localStorage.clear()
-            drowList()
-        }
-        
-    })
+        const liItem = document.createElement('li')
+        liItem.classList = 'list-item'
+        list.append(liItem)
 
-    liItem.append(pItem, buttonDone, buttonRem)
+        const pItem = document.createElement('p')
+        pItem.classList.add('list-content')
+        pItem.innerText = todoItem.text
+        if (todoItem.done) {pItem.classList.add('taskDone')}
+
+        const buttonDone = document.createElement('button')
+        buttonDone.classList.add('list-btn', 'done', 'btn')
+        buttonDone.innerText = 'done'
+        buttonDone.addEventListener('click', () => {
+            todoItem.done = !todoItem.done
+            drowList()
+        })
+
+        const buttonRem = document.createElement('button')
+        buttonRem.classList.add('list-btn', 'remove', 'btn')
+        buttonRem.innerText = 'remove'
+        buttonRem.addEventListener('click', () => {
+            const curInd = todoList.indexOf(todoItem)
+            todoList.splice(curInd, 1)
+            if (todoList.length) {
+                localStorage.setItem('todo', JSON.stringify(todoList))
+                drowList()
+            } else {
+                localStorage.clear()
+                drowList()
+            }
+
+        })
+
+        liItem.append(pItem, buttonDone, buttonRem)
     }
 }
