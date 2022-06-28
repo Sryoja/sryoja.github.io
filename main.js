e4ea9cbdb3142692808ee75072923b64bd1d25c1
Sergey <dodzeek@gmail.com>
2022-06-28 18:25:37 +0500

    ver 28.06.22

--- main.js
+++
@@ -1,8 +0,0 @@
-
-const menuBtn = document.getElementById('menuBtn')
-const headerWrap = document.querySelector('.headerWrap')
-
-
-menuBtn.addEventListener('click', function(){
-    headerWrap.classList.toggle('_mobile')
-})
